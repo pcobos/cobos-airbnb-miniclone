163240c68dce3d4f72e49b4e2827215d7452a233
Pedro Cobos <pedro.cobos@live.com>
2020-08-10 19:39:05 -0500

    added index, show, new and create actions to flat controller. Added bootstrap gem and simple form gem as well

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Flat.destroy_all
+puts "> Creating flats..."
+
+5.times do 
+  Flat.create!(
+  name: Faker::Cannabis.brand,
+  address: Faker::Address.street_address,
+  description: "Beautiful apartment in the heart of the city. Easy access to metro",
+  price_per_night: rand(150..250),
+  number_of_guests: rand(2..5)
+)
+end
+
+puts "> Done!"
